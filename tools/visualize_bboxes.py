8081cfa901ea3400af556e766954310760b4c1bb
Kimia <kajalafshari@yahoo.com>
2023-11-19 00:24:24 -0800

    modifying path to get bbox vis

--- tools/visualize_bboxes.py
+++ tools/visualize_bboxes.py
@@ -11,8 +11,8 @@ import os
 #############################################################################
 # question_id = "Interaction_T1_4"
 # video_id = "TJZ0P"
-question_id = "Interaction_T1_5568"
-video_id = "XSS0J"
+question_id = "Interaction_T1_5590"
+video_id = "OJ4IE"
 path_to_videos = "../../data/star"
 # Output directory. The frames with the bounding boxes will be saved here
 output_dir = "../../data/star/dev"
