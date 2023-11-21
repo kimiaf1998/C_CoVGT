d1aefcc3ade8ab91e1f870d780ad7b118e33c9bc
Kimia <kajalafshari@yahoo.com>
2023-11-20 19:43:44 -0800

    added star train shell

---
+++ shells/star_train.sh
@@ -0,0 +1,26 @@
+GPU=$1
+CUDA_VISIBLE_DEVICES=$GPU python main.py --checkpoint_dir=star \
+	--dataset=star \
+	--mc=5 \
+	--bnum=5 \
+	--epochs=20 \
+	--lr=0.00001 \
+	--qmax_words=30 \
+	--amax_words=38 \
+	--max_feats=32 \
+	--batch_size=64 \
+	--batch_size_val=64 \
+	--num_thread_reader=8 \
+	--mlm_prob=0 \
+	--cl_loss=1 \
+	--n_layers=1 \
+	--embd_dim=512 \
+	--ff_dim=1024 \
+	--dropout=0.3 \
+	--seed=666 \
+	--lan="RoBERTa" \
+	--save_dir='../data/save_models/star/CoVGT/' \
+	#--pretrain_path=../data/save_models/webvid180K/co_e1.pth \
+	
+	
+	
