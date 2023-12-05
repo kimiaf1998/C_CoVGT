fixed masking and tubedetr shapes

dataloader/cvqa_loader.py
@@ -238,7 +238,6 @@ class VideoQADataset(Dataset):
 
         numc, numf, numr, d_model = video_o.shape
         vid_duration = numc
-        num_objects = np.full((numc*numf, 1), self.bbox_num)
         # extract topk regions
         # video_o = video_o[:,:,:5,:]
 
@@ -379,7 +378,7 @@ class VideoQADataset(Dataset):
             "video_o": video_o,
             "video_f": video_f,
             "video_len": vid_duration,
-            "object_len": num_objects,
+            "object_len": self.bbox_num,
             "question": question_embd,
             "question_txt": question_txt,
             "type": qtype,

model/CoVGT.py
@@ -640,6 +640,7 @@ class VGT(nn.Module):
         video_mask=None,
         text_mask=None,
         object_mask=None,
+        att_mask=None,
         max_seq_len=0,
         seg_feats = None,
         seg_num = None,
@@ -713,22 +714,21 @@ class VGT(nn.Module):
 
 
                 # add learnable positional embedding
-                video_proj = self.position_v(video_proj)    #(bs, numc, dmodal)
+                video_proj = self.position_v(video_proj)    # (bs, numc, dmodal)
                 # global transformer to capture temporal relations between video contents
-                attended_v = self.mmt(x=video_proj, attn_mask=video_mask)[0]
+                attended_v = self.mmt(x=video_proj, attn_mask=video_mask)[0] # (bs, numc, dmodal)
 
                 # predict answer bboxes (tube decoder)
                 video_o, _ = video[0], video[1]
-
-                bsize, numc, numf, numr, fdim = video_o.size()  # (bsize, n clips,n frames, n regions, feat dim)
-
                 X = self.encode_vid(video_o)  # (bs, numc*numf, numr, dmodel)
-                X = X.view(bsize, numc, numf, numr, -1)
 
-
-                # TODO use tube output and return as  head predictions
+                # TODO use tube output and return as head predictions
+                print("X shape:", X.shape)
+                print("vt shape:", attended_v.shape)
+                print("object_mask shape:", object_mask.shape)
+                print("video_mask shape:", video_mask.shape)
                 tube = self.tube_detector(object_encoding=X, vt_encoding=attended_v,
-                                          query_mask=object_mask, vt_mask=video_mask)
+                                          object_mask=object_mask, vt_mask=video_mask.bool())
 
                 # TODO add localization loss
 

model/position_encoding.py
@@ -24,7 +24,7 @@ class TimeEmbeddingLearned(nn.Module):
         nn.init.uniform_(self.time_embed.weight)
 
     def forward(self, ln):
-        return self.time_embed.weight[:ln].unsqueeze(1) # 1x(num_queries)xd_model
+        return self.time_embed.weight[:ln].unsqueeze(1) # lnx1xd_model (tx1xdmodel)
 
 
 class TimeEmbeddingSine(nn.Module):

model/space_time_decoder.py
@@ -97,20 +97,27 @@ class Transformer(nn.Module):
 
     def forward(
         self,
-        query_embed,
+        query_encoding,
         vt_encoding,
         query_mask=None,
         vt_mask=None,
+        video_max_len=32,
     ):
 
         # time-space-text attention
-        # t = numc*numf
+        n_queries, bt, d_model = query_encoding.size()
+        bsize = bt//video_max_len
+        time_pos = self.time_embed(video_max_len).repeat(bsize, n_queries,1).transpose(0,1)
+
+        print("** SPACE TIME DECODER **")
+        print("query_encoding shape:", query_encoding.shape)
+        print("time_pos shape:", time_pos.shape)
         hs = self.decoder(
-            query_embed=query_embed,  # n_queriesx(bsize*t)xd_model
-            vt_encoding=vt_encoding,  # (bsize*t)x1xd_model
+            query_encoding=query_encoding,  # n_queriesx(bsize*t)xd_model
+            vt_encoding=vt_encoding,  # 1x(bsize*t)xd_model
             query_mask=query_mask,  # (bsize*t)xn_queries
             vt_mask=vt_mask, # (bsizext)x1
-            query_pos=self.time_embed, # n_queriesxd_model
+            query_pos=time_pos, # (bsize*t)xn_queriesxd_model
         )  # n_layersxn_queriesx(bsize*t)xdmodel
         if self.return_weights:
             hs, weights, cross_weights = hs
@@ -234,6 +241,9 @@ class TransformerDecoderLayer(nn.Module):
 
         q = self.with_pos_embed(query, query_pos)
         k = v = query
+        print("query shape:", query.shape)
+        print("vt_encoding shape:", vt_encoding.shape)
+        print("vt_mask shape:", vt_mask.shape)
         # Temporal Self attention
         tgt2, weights = self.self_attn(
             q,
@@ -259,8 +269,7 @@ class TransformerDecoderLayer(nn.Module):
             query=self.with_pos_embed(tgt_cross, query_pos_cross),
             key=vt_encoding,
             value=vt_encoding,
-            attn_mask=att_mask,
-            key_padding_mask=vt_mask,
+            # key_padding_mask=vt_mask, # TODO expecting key_padding_mask shape of (2048, 1), but got torch.Size([64, 8])
         ) # 1x(b*t)xf
 
         tgt2 = tgt2.view(b, t, f).transpose(0, 1)  # 1x(b*t)xf -> bxtxf -> txbxf
@@ -293,7 +302,7 @@ def _get_clones(module, N):
 
 def build_transformer(args):
     return Transformer(
-        d_model=args.loc_hidden_dim,
+        d_model=args.embd_dim,
         dropout=args.dropout,
         nhead=args.n_heads,
         dim_feedforward=args.ff_dim,

model/tubedetr.py
@@ -79,8 +79,8 @@ class TubeDecoder(nn.Module):
         self,
         object_encoding, # (bs, t, numr, dmodel)
         vt_encoding, # (bs, numc, dmodel)
-        object_mask, # (bs, t, numr)
-        vt_mask, # (bs, t)
+        object_mask, # (numr, numr)
+        vt_mask, # (bs, numc)
     ):
         """The forward expects a NestedTensor, which consists of:
            - samples.tensor: batched frames, of shape [n_frames x 3 x H x W]
@@ -101,12 +101,14 @@ class TubeDecoder(nn.Module):
         _, numc, _ = vt_encoding.size()
         numf = t//numc
         query_encoding = object_encoding.view(num_queries, bs*t, -1) # (num_queries, bs*t, dmodel)
-        vt_encoding = vt_encoding.view(1, bs*numc, -1).repeat(numf, 1, 1) # (1, bs*t, dmodel)
+        vt_encoding = vt_encoding.flatten(0,1).unsqueeze(0).repeat(1, numf, 1) # (bs, numc, dmodel) -> (bs*numc, dmodel)
+                                                                            # -> (1, bs*numc, dmodel)-> (1, bs*t, dmodel)
+        vt_mask.reshape(bs * numc, -1).repeat(numf, 1) # (bs, numc) -> (bs*numc, 1) -> (bs*t,1)
         hs = self.transformer(
             query_encoding=query_encoding,  # (num_queries)x(BT)xF
             vt_encoding=vt_encoding,  # (1)x(BT)xF
-            query_mask=object_mask.view(bs, -1),  # Bx(Txnum_queries)
-            vt_mask=vt_mask.view(bs*t, -1),  # (BT)x1
+            query_mask=object_mask,  # num_queriesxnum_queries)
+            vt_mask=vt_mask,  # (BT)x1
         )
         if self.guided_attn:
             hs, weights, cross_weights = hs
@@ -116,10 +118,11 @@ class TubeDecoder(nn.Module):
         if self.sted:
             outputs_sted = self.sted_embed(hs)
 
-        hs = hs.flatten(2, 3)  # n_layersxnum_queriesxbxtxf -> n_layersxnum_queriesx(b*t)xf
+        print("hs shape:", hs.shape)
+        hs = hs.flatten(1, 2)  # n_layersx(b*t)xnum_queriesxf
 
         outputs_coord = self.bbox_embed(hs).sigmoid()
-        out.update({"pred_boxes": outputs_coord[-1]}) # fetch last-layer output -> num_queriesx(b*t)x1
+        out.update({"pred_boxes": outputs_coord[-1]}) # fetch last-layer output ->  n_layersx(b*t)xnum_queriesxfx1
         if self.sted:
             out.update({"pred_sted": outputs_sted[-1]})
         if self.guided_attn:
@@ -140,7 +143,7 @@ class TubeDecoder(nn.Module):
                 if self.guided_attn:
                     out["aux_outputs"][i_aux]["weights"] = weights[i_aux]
                     out["aux_outputs"][i_aux]["ca_weights"] = cross_weights[i_aux]
-
+        
         return out
 
 

train/train_covgt.py
@@ -28,19 +28,23 @@ def eval(model, data_loader, a2v, args, test=False, tokenizer="RoBERTa"):
                 batch['seg_feats'].cuda(),
                 batch['seg_num']
             )
-           
+
             video_len = batch["video_len"]
-            object_len = batch["object_len"] # TODO obj len = 10 not a loist
+            max_object_len = max(batch["object_len"])
             seq_len = batch["seq_len"]
-           
+
             question_mask = (question!=tokenizer.pad_token_id).float() #RobBERETa
             answer_mask = (answer!=tokenizer.pad_token_id).float() #RobBERETa
 
             print("video_o shape:", video_o.size())
+            bs, _, _, max_object_num, _ = video_o.size()
             video_mask = get_mask(video_len, video_o.size(1)).cuda()
             print("video_mask:", video_mask.size())
-            object_mask = get_mask(object_len, video_o.size(3), dim=2).cuda()
-            print("object_mask:", object_mask.size())
+            # object_mask = get_mask(object_len.flatten(0,1), max_object_num).bool().cuda()
+            print("object_len:", max_object_len)
+            object_mask = (torch.arange(max_object_len).unsqueeze(1).to(video_o.device) < video_o.size(2)).repeat(1, max_object_len)
+            print("object_mask:", object_mask)
+            print("object_mask size:", object_mask.size())
             count += answer_id.size(0)
             video = (video_o, video_f)
             if not args.mc:
@@ -80,16 +84,17 @@ def eval(model, data_loader, a2v, args, test=False, tokenizer="RoBERTa"):
                     question,
                     text_mask=answer_mask,
                     video_mask=video_mask,
+                    object_mask=object_mask,
                     answer=answer,
                     seq_len = seq_len,
                     seg_feats = seg_feats,
                     seg_num = seg_num
                 )
-                # predicts = fusion_proj.squeeze() 
-                
+                # predicts = fusion_proj.squeeze()
+
                 fusion_proj = fusion_proj.unsqueeze(2)
                 predicts = torch.bmm(answer_proj, fusion_proj).squeeze()
-                
+
                 predicted = torch.max(predicts, dim=1).indices.cpu()
                 metrics["acc"] += (predicted == answer_id).sum().item()
                 for bs, qid in enumerate(question_id):
@@ -128,7 +133,7 @@ def train(model, train_loader, a2v, optimizer, criterion, scheduler, epoch, args
             batch['qsn_seq_len']        # length of qsns token ids
         )
         video_len = batch["video_len"]
-        object_len = batch["object_len"]
+        max_object_len = max(batch["object_len"])
 
         question_mask = (question != tokenizer.pad_token_id).float().cuda() #RobBERETa
         answer_mask = (answer!=tokenizer.pad_token_id).float().cuda() #RobBERETa
@@ -138,9 +143,11 @@ def train(model, train_loader, a2v, optimizer, criterion, scheduler, epoch, args
         print("video_o shape:", video_o.size())
         video_mask = get_mask(video_len, video_o.size(1)).cuda()
         print("video_mask:", video_mask.size())
-        object_mask = get_mask(video_o.size(2), object_len, dim=2).cuda()
-        print("object_mask:", object_mask.size())
-       
+        # object_mask = get_mask(video_o.size(2), object_len).bool().cuda()
+        object_mask = (torch.arange(max_object_len).unsqueeze(1).to(video_o.device) < video_o.size(2)).repeat(1, max_object_len)
+        print("object_mask:", object_mask)
+        print("object_mask size:", object_mask.size())
+
         qsn_mask = (qsn_token_ids != tokenizer.pad_token_id).float().cuda()
         
         video = (video_o, video_f)