fixed eval + visualization scaling

demo_star.py
@@ -2,6 +2,7 @@ import argparse
 
 import torch
 import os
+import shutil
 import os.path as osp
 import numpy as np
 from args import get_args, get_parser
@@ -12,11 +13,9 @@ from transformers import RobertaTokenizerFast
 
 from models.Tube_CoVGT import build_model
 from tools.object_align import align
-from tools.postprocess import PostProcess
-from tools.util import tokenize, load_file, transform_bb, load_model_by_key
+from util import tokenize, load_file, transform_bb, load_model_by_key, get_mask
 from tools.box_ops import box_cxcywh_to_xyxy
 from tools.bbox_visualizer import draw_and_save_rects
-from util import get_mask
 
 video_path = f'/data/kimia/hdd3_mount/kimia/data/STAR/frames_orig_fps'
 
@@ -29,8 +28,7 @@ def get_video_info(video_name, qid):
     roi_feats, roi_bboxs = [], []
     video_frame_ids = []
     fid = vid_clips[0][0]
-    # features indices starts from 0 while frames 1
-    img = Image.open(f'{video_path}/{video_name}/{int(fid) + 1:06}.png')
+    img = Image.open(f'{video_path}/{video_name}/{fid}.png')
     width, height = img.size
     img.close()
 
@@ -39,7 +37,8 @@ def get_video_info(video_name, qid):
         clip_frame_ids = []
         for fid in clip:
             clip_frame_ids.append(fid)
-            fid = int(fid)
+            fid = int(fid) - 1
+            # features indices starts from 0 while frames 1
             frame_feat_file = osp.join(video_feature_path, f'frame_feat/{video_name}/{fid:06d}.npy')
             frame_feat = np.load(frame_feat_file)
             clip_feat.append(frame_feat)
@@ -83,8 +82,6 @@ if __name__ == "__main__":
     model.to(device)
     print("models loaded")
 
-    postprocessors = PostProcess()
-
     # load checkpoint
     assert args.load
     model.load_state_dict(load_model_by_key(model, args.load, device))
@@ -124,7 +121,7 @@ if __name__ == "__main__":
     video_o = video_o.unsqueeze(0).to(device)
     samples = (video_o, video_f)
     questions = torch.tensor([0], dtype=torch.long).unsqueeze(0).to(device)
-    vid_orig_size = torch.tensor(vid_orig_size).unsqueeze(0)
+    w, h = vid_orig_size
 
     answer_mask = (ans_token_ids != tokenizer.pad_token_id).float().to(device)  # RobBERETa
     video_mask = get_mask(torch.tensor([video_o.size(1)]).to(device), video_o.size(1))
@@ -154,9 +151,8 @@ if __name__ == "__main__":
         output.update({'question': question_txt, 'prediction': pred, 'answer': answer_txt})
 
         # convert predicts from relative [0, 1] to absolute [0, height] coordinates
-        tube_pred["pred_boxes"] = box_cxcywh_to_xyxy(tube_pred["pred_boxes"])
-        results = PostProcess()(tube_pred, vid_orig_size).to(device)# 1x32x10x4
-        results = results[:,0,:].unsqueeze(1)
+        results = box_cxcywh_to_xyxy(tube_pred["pred_boxes"]) * torch.tensor([w,h,w,h]) # 1x32x10x4
+        results = results[:,0,:].unsqueeze(1).to(device)
 
         bbox_res = {}  # maps image_id to the coordinates of the detected box
         video_frame_ids = np.asarray(video_frame_ids).reshape(-1)
@@ -175,6 +171,11 @@ if __name__ == "__main__":
         video_save_path = os.path.join(
                             args.save_dir,
                             video_id)
+
+        if os.path.exists(video_save_path):
+            shutil.rmtree(video_save_path)
+            
+        os.makedirs(video_save_path)
         # extract actual images from the video to process them adding boxes
         draw_and_save_rects(os.path.join(video_path, video_id), video_frame_ids, results, video_save_path)
 

main.py
@@ -15,7 +15,6 @@ from util import compute_a2v, load_model_by_key, save_to, plot_and_save_epochs_r
 from dataloader.cvqa_loader import get_videoqa_loaders
 from train.train_covgt import train, eval
 from tools.bbox_visualizer import draw_and_save_rects, add_label
-from tools.postprocess import PostProcess
 from tqdm import tqdm
 from tools.box_ops import box_cxcywh_to_xyxy
 
@@ -79,7 +78,7 @@ def main(args):
     
     if args.pretrain_path != "":
         # models.load_state_dict(torch.load(args.pretrain_path))
-        model.load_state_dict(load_model_by_key(model, args.pretrain_path))
+        model.load_state_dict(load_model_by_key(model, args.pretrain_path, args.device))
         logging.info(f"Loaded checkpoint {args.pretrain_path}")
     logging.info(
         f"Nb of trainable params:{sum(p.numel() for p in model.parameters() if p.requires_grad)}"
@@ -206,54 +205,38 @@ def main(args):
             pred_boxes = preds['box']
             gt_text = gt['desc']
             gt_boxes = gt['box']
-            pred_boxes_cp = pred_boxes.copy()
-            gt_boxes_cp = gt_boxes.copy()
 
             from PIL import Image
             img = Image.open(osp.join(args.video_dir, video_id, f'{video_frame_ids[0]:06}.png'))
-            vid_orig_size = torch.tensor(img.size)
             w, h = img.size
-            if video_id == "7MRKY":
-                print("orig size:", vid_orig_size)
-                pred_boxes = torch.tensor(pred_boxes)
-                gt_boxes = torch.tensor(gt_boxes)
-                if pred_boxes.ndim == 3:
-                    pred_boxes = pred_boxes[:, 0, :]
-                # pred_boxes = PostProcess()(pred_boxes, vid_orig_size.repeat(pred_boxes.shape[0], 1))  # 32x10x4
-                #
-                # gt_boxes = PostProcess()(gt_boxes, vid_orig_size.repeat(gt_boxes.shape[0], 1))  # 32x10x4
+            pred_boxes = torch.tensor(pred_boxes)
+            gt_boxes = torch.tensor(gt_boxes)
+            if pred_boxes.ndim == 3:
+                pred_boxes = pred_boxes[:, 0, :]
 
-                pred_boxes = pred_boxes* torch.tensor([w,h,w,h])
-                gt_boxes = gt_boxes* torch.tensor([w,h,w,h])
-                print("GT:", gt_boxes)
+            pred_boxes = pred_boxes * torch.tensor([w,h,w,h])
+            gt_boxes = gt_boxes * torch.tensor([w,h,w,h])
 
-                total_boxes = np.stack((gt_boxes.numpy(), pred_boxes.numpy()), axis=1)
+            video_save_path = os.path.join(
+                args.save_dir,
+                video_id,
+                q_id)
+            if not os.path.exists(video_save_path):
+                os.makedirs(video_save_path)
+            # extract actual images from the video to process them adding boxes
+            draw_and_save_rects(osp.join(args.video_dir, video_id), video_frame_ids, pred_boxes.unsqueeze(1), video_save_path)
+            draw_and_save_rects(video_save_path, video_frame_ids, gt_boxes.unsqueeze(1), video_save_path)
 
-                video_save_path = os.path.join(
-                    args.save_dir,
-                    video_id,
-                    q_id)
-                if not os.path.exists(video_save_path):
-                    os.makedirs(video_save_path)
-                # extract actual images from the video to process them adding boxes
-                draw_and_save_rects(osp.join(args.video_dir, video_id), video_frame_ids, total_boxes, video_save_path) # TODO draw gt as well
+            # Add question/answer/preds
+            file_path = osp.join(video_save_path, 'pred.txt')
 
-                # Add question/answer/preds
-                file_path = osp.join(video_save_path, 'pred.txt')
+            # Open the file in write mode ('w')
+            with open(file_path, 'w') as file:
+                # Write some text to the file
+                file.write(question+" "+gt_text+ "\n")
+                file.write("pred: "+pred_text + "\n")
 
-                # Open the file in write mode ('w')
-                with open(file_path, 'w') as file:
-                    # Write some text to the file
-                    file.write(question+" "+gt_text+ "\n")
-                    file.write("pred: "+pred_text + "\n")
-
-                print(f"Video saved in {video_save_path}")
-                for i, box in enumerate(pred_boxes):
-                    print("pred_box orig", pred_boxes_cp[i])
-                    print("pred_box scaled",box)
-                    print("gt orig", gt_boxes_cp[i])
-                    print("gt box scaled",gt_boxes[i])
-                break
+            print(f"Video saved in {video_save_path}")
 
 
 

tools/bbox_visualizer.py
@@ -363,10 +363,10 @@ def draw_multiple_flags_with_labels(img,
 
 
 def draw_and_save_rects(video_path, frame_ids, boxes, video_save_path):
-    colors = np.random.rand(boxes.shape[1], 3)  # 3 for RGB components
+    colors = np.random.randint(0, 256, size=(boxes.shape[1], 3)).tolist()  # 3 for RGB components
     for idx, frm_id in enumerate(frame_ids):
         # load extracted image
-        frm_id = f'{frm_id:06}'
+        frm_id = f'{int(frm_id):06}'
         img_path = os.path.join(
             video_path,
             frm_id + ".png"

util.py
@@ -36,12 +36,10 @@ def tokenize(
     tokens = ''
     return torch.tensor(token_ids, dtype=torch.long), tokens
 
-def transform_bb(roi_bbox, width, height, a=False):
+def transform_bb(roi_bbox, width, height):
     dshape = list(roi_bbox.shape)
     tmp_bbox = roi_bbox.reshape([-1, 4])
     relative_bbox = tmp_bbox / np.asarray([width, height, width, height])
-    if a:
-        print("reltive:", relative_bbox.reshape(dshape))
     relative_area = (tmp_bbox[:, 2] - tmp_bbox[:, 0] + 1) * \
                     (tmp_bbox[:, 3] - tmp_bbox[:, 1] + 1)/ (width*height)
     relative_area = relative_area.reshape(-1, 1)
@@ -368,8 +366,8 @@ def group(csv_data, gt=True):
     return ans_group, qsn_group
 
 
-def load_model_by_key(cur_model, model_path):
-    model_dict = torch.load(model_path)
+def load_model_by_key(cur_model, model_path, device):
+    model_dict = torch.load(model_path, map_location=torch.device(device))
     new_model_dict = {}
     for k, v in cur_model.state_dict().items():
         if k in model_dict: